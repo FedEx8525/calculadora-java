d49b3acef6c1d5bc330c4db65703c64151afad3f
Federico Cantore <f.cantore85@gmail.com>
2026-03-24 11:34:53 +0100

    feat: add base structuro of Calculadora

---
+++ src/com/calculadora/Calculadora.java
@@ -0,0 +1,12 @@
+package com.calculadora;
+
+//Taller de conflictos
+public class Calculadora {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
+
+        System.out.println("---Calculadora---");
+        System.out.println("Operation: " + (a + b));
+    }
+}
